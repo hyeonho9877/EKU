f3901646a07ff486d6efceb75290deea06c9962b
ldongyeop2 <62781771+ldongyeop2@users.noreply.github.com>
2022-04-02 17:30:41 +0900

    회원가입

--- eku/app/src/main/java/com/kyonggi/eku/SignInfoActivity.java
+++ eku/app/src/main/java/com/kyonggi/eku/SignInfoActivity.java
@@ -3,12 +3,18 @@ package com.kyonggi.eku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignInfoActivity extends AppCompatActivity {
+
+    Button btn_submit;
+    Button btn_cancle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_info);
     }
+
+
 }
---
+++ eku/app/src/main/java/com/kyonggi/eku/Student.java
@@ -0,0 +1,10 @@
+package com.kyonggi.eku;
+
+public class Student {
+
+    int std_no;
+    String std_dept;
+    String std_email;
+    String std_name;
+
+}
