3517917cf75e74d97050ec129cb6c8364a679c12
Woong <jiyko7@kyonggi.ac.kr>
2022-04-02 14:44:02 +0900

    스와이프 기능 추가
    스와이프 기능
    커뮤니티 페이지 추가중

--- eku/app/src/main/java/com/kyonggi/eku/MainBoard.java
+++ eku/app/src/main/java/com/kyonggi/eku/MainBoard.java
@@ -4,21 +4,102 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainBoard extends AppCompatActivity {
 
     String[] items = {"1강의동","2강의동","3강의동","4강의동","5강의동","6강의동","7강의동","8강의동","9강의동","제2공학관"};
+    GestureDetector gestureDetector = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_board);
+
+        View gestureView = findViewById(R.id.gestureView);
+        gestureView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                gestureDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+        gestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            ViewConfiguration configuration = ViewConfiguration.get(getApplicationContext());
+            final int minSwipeDelta = configuration.getScaledPagingTouchSlop();
+            final int minSwipeVelocity = configuration.getScaledMinimumFlingVelocity();
+            final int maxSwipeVelocity = configuration.getScaledMaximumFlingVelocity();
+
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float velocityX, float velocityY) {
+                boolean result = false;
+                try {
+                    float deltaX = motionEvent1.getX() - motionEvent.getX();
+                    float deltaY = motionEvent1.getY() - motionEvent.getY();
+                    float absVelocityX = Math.abs(velocityX);
+                    float absVelocityY = Math.abs(velocityY);
+                    float absDeltaX = Math.abs(deltaX);
+                    float absDeltaY = Math.abs(deltaY);
+                    if (absDeltaX > absDeltaY) {
+                        if (absDeltaX > minSwipeDelta && absVelocityX > minSwipeVelocity
+                                && absVelocityX < maxSwipeVelocity) {
+                            if (deltaX < 0) {
+                                onSwipeLeft();
+                            } else {
+                                onSwipeRight();
+                            }
+                        }
+                        result = true;
+                    } else if (absDeltaY > minSwipeDelta && absVelocityY > minSwipeVelocity
+                            && absVelocityY < maxSwipeVelocity) {
+                        if (deltaY < 0) {
+                            onSwipeTop();
+                        } else {
+                            onSwipeBottom();
+                        }
+                    }
+                    result = true;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return result;
+
+            }
+        });
 
         Spinner spinner = (Spinner)findViewById(R.id.board_Spinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(
@@ -68,5 +149,30 @@ public class MainBoard extends AppCompatActivity {
             PreferenceManagers.removeKey(getApplicationContext(),"time"+j);
         }
         PreferenceManagers.setInt(getApplicationContext(), "board_Count", 0);
+    }
+    public void onSwipeLeft() {
+        Toast.makeText(this,"좌측 스와이프", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getApplicationContext(), MainCommunity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onSwipeRight() {
+        Toast.makeText(this,"우측 스와이프", Toast.LENGTH_SHORT).show();
+        /*   ---------------------여기 지도 activity 넣어주셈-------------------------
+        Intent intent = new Intent(getApplicationContext(), 지도.class);
+        startActivity(intent);
+        finish();
+
+         */
+
+    }
+
+    public void onSwipeTop() {
+        Toast.makeText(this,"상단 스와이프", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onSwipeBottom() {
+        Toast.makeText(this,"하단 스와이프", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ eku/app/src/main/java/com/kyonggi/eku/MainCommunity.java
@@ -0,0 +1,14 @@
+package com.kyonggi.eku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCommunity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_community);
+    }
+}
