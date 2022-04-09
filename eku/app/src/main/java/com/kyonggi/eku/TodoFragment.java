f19c8ff0d41f9d4ee61947210e283aa4d5e721dc
ldongyeop2 <62781771+ldongyeop2@users.noreply.github.com>
2022-04-09 18:41:04 +0900

    todolist 수정

---
+++ eku/app/src/main/java/com/kyonggi/eku/TodoFragment.java
@@ -0,0 +1,24 @@
+package com.kyonggi.eku;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TodoFragment extends Fragment {
+    private static final String TAG = "TodoFragment";
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_todo, container, false);
+
+        initUI(rootView);
+
+        return rootView;
+    }
+
+    private void initUI(ViewGroup rootView){
+
+    }
+}
