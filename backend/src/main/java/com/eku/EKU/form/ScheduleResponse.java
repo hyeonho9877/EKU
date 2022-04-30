3074f7a2ee0f2790801f2af93e334ce788b4ab46
iz100412 <iz100412@gmail.com>
2022-04-30 19:22:20 +0900

    Revert "Merge branch 'main' into lhg"
    This reverts commit 3733940054a84d27b2727002a830a071943a847f, reversing
    changes made to 497f3c1b59594d5bd04e06e7af70d5d4eb423b8e.

--- backend/src/main/java/com/eku/EKU/form/ScheduleResponse.java
+++
@@ -1,9 +0,0 @@
-package com.eku.EKU.form;
-
-import lombok.Data;
-
-@Data
-public class ScheduleResponse {
-    private long studNo;
-    private long password;
-}
--- backend/src/main/java/com/eku/EKU/repository/LectureRepository.java
+++ backend/src/main/java/com/eku/EKU/repository/LectureRepository.java
@@ -3,8 +3,5 @@ package com.eku.EKU.repository;
 import com.eku.EKU.domain.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-    Optional<Lecture> findByPrototypeLectureNameAndPrototypeProfessor(String time, String professor);
 }
