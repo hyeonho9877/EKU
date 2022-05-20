d5679a62ba6ad3f6db61e70f664ef4150bf85188
Polite-Jehu <werber112@gmail.com>
2022-05-20 23:48:02 +0900

    도난방지 및 메인업그레이드
    업그레이드 완료

--- eku/app/src/main/java/com/kyonggi/eku/MainBoard.java
+++ eku/app/src/main/java/com/kyonggi/eku/MainBoard.java
@@ -52,7 +52,7 @@ import java.util.HashMap;
 public class MainBoard extends AppCompatActivity {
 
     public static final int Minorcheck = 61686;
-    String[] showBuilding = {"1강의동","2강의동","3강의동","4강의동","5강의동","6강의동","7강의동","8강의동","9강의동","제2공학관"};
+    String[] showBuilding = {"1강의동","2강의동","3강의동","4강의동","5강의동","6강의동","7강의동","8강의동","9강의동","제2공학관","종합강의동"};
     int buildingSelected = 0;
     int[] building = {1,2,3,4,5,6,7,8,9,0};
     AlertDialog buildingSelectDialog;
