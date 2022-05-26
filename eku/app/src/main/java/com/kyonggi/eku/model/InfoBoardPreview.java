e5b46dba51751e0303897db3cc0c35c43b2a1517
hyeonho <6190606@naver.com>
2022-05-26 16:11:46 +0900

    update: ui-update

--- eku/app/src/main/java/com/kyonggi/eku/model/InfoBoardPreview.java
+++ eku/app/src/main/java/com/kyonggi/eku/model/InfoBoardPreview.java
@@ -2,7 +2,10 @@ package com.kyonggi.eku.model;
 
 import androidx.annotation.NonNull;
 
+import java.util.List;
+
 public class InfoBoardPreview extends BoardPreview{
+    private List<String> imageList;
     public InfoBoardPreview(BoardPreview preview) {
         super(preview.getId(), preview.getWriter(), preview.getTitle(), preview.getNo(), preview.getTime(), preview.getView());
     }
@@ -11,5 +14,13 @@ public class InfoBoardPreview extends BoardPreview{
     @Override
     public String toString() {
         return "InfoBoardPreview(id="+getId()+", writer="+getWriter()+", title="+getTitle()+", no="+getNo()+", time="+getTime()+", view="+getView();
+    }
+
+    public List<String> getImageList() {
+        return imageList;
+    }
+
+    public void setImageList(List<String> imageList) {
+        this.imageList = imageList;
     }
 }
